5a13c4ba7c18ed85ef27aa68a4fb0478e57cc0a7
arslankhansna <66205041+arslankhansna@users.noreply.github.com>
2025-06-24 17:33:17 +0530

    Create skills.js

---
+++ pages/skills.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+export default function Skills() {
+  return (
+    <Layout>
+      <h1>Skills</h1>
+      <p><strong>Languages:</strong> HTML, CSS, JavaScript, Python, SQL</p>
+      <p><strong>Tools:</strong> Figma, Adobe Suite, LaTeX, Canva, MS Office</p>
+      <p><strong>Topics:</strong> AI, Data Science, Networking, SEO, UI/UX</p>
+    </Layout>
+  );
+}
