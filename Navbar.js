f5b5643c1ab92b97756d58610c584136efac90aa
arslankhansna <66205041+arslankhansna@users.noreply.github.com>
2025-06-24 17:17:40 +0530

    Create Navbar.js

---
+++ Navbar.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import styles from './Navbar.module.css'; // create this for custom styling
+
+export default function Navbar() {
+  return (
+    <nav className={styles.nav}>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/projects">Projects</Link>
+      <Link href="/skills">Skills</Link>
+      <Link href="/certifications">Certifications</Link>
+      <Link href="/contact">Contact</Link>
+    </nav>
+  );
+}
