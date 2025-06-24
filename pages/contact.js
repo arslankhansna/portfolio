5dc6ecb1095e49c55f3811e6febd0a53ef4a1408
arslankhansna <66205041+arslankhansna@users.noreply.github.com>
2025-06-24 17:37:42 +0530

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+export default function Contact() {
+  return (
+    <Layout>
+      <h1>Contact Me</h1>
+      <p>Email: <a href="mailto:arslankhansna@gmail.com">arslankhansna@gmail.com</a></p>
+      <p>Location: Bengaluru, India</p>
+    </Layout>
+  );
+}
