f3a7b4336f7b4bbd90b750c33b02c1fda4e47506
arslankhansna <66205041+arslankhansna@users.noreply.github.com>
2025-06-24 17:34:17 +0530

    Create certifications.js

---
+++ pages/certifications.js
@@ -0,0 +1,17 @@
+import Layout from '../components/Layout';
+
+export default function Certifications() {
+  return (
+    <Layout>
+      <h1>Certifications</h1>
+      <ul>
+        <li>Python for Data Science — IBM</li>
+        <li>Digital Marketing — Google</li>
+        <li>SEO in Google — Google</li>
+        <li>Discrete Mathematics — UC San Diego</li>
+        <li>Computer Networking — Internshala</li>
+        <li>Writing in the Sciences — Excelsior College</li>
+      </ul>
+    </Layout>
+  );
+}
